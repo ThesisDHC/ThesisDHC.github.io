23f8adbcd189c8680e079d03c57c1608647227b3
ThesisDHC <luna.niemann@outlook.com>
2025-06-24 15:35:34 +0200

    Update script2.js

--- script2.js
+++ script2.js
@@ -14,9 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
             avatar: 'media/Profilepic_sarcasmonly.jpg',
             mediaSrc: ['media/Reel1_WW3.mp4'],
             caption: 'Hold on guys, we got this😅',
-            likes: 836.460,
+            likes: '836.460',
             comments: [
-                { user: 'piips_', text: 'And then getting asked: why u don't want to have kids?' },
+                { user: 'piips_', text: 'And then getting asked: why u don\'t want to have kids?' },
                 { user: 'nibhaille', text: 'Cannot even buy a house to enjoy this carnage in' }
             ]
         },
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
             avatar: 'media/Profilepic_MH.jpg',
             mediaSrc: ['media/Debunking1.jpg', 'media/Debunking2_Post_Pills.jpg', 'media/Debunking3_Reel_Holistically.jpg', 'media/Debunking4_Reel_Gutsupplements.jpg', 'media/Debunking5_Reel_Yoga.jpg', 'media/Debunking6.jpg'  ],
             caption: 'Always consult your therapist and physician. These institutions provide additional reliable information: the World Health Organization, the National Health Service UK, or the National Institutes of Health #debunking #mentalhealthfacts #nomyths #nostigma',
-            likes: 5.328,
+            likes: '5.328',
             comments: [
                 { user: 'juliexx', text: 'So helpful!' },
                 { user: 'marcello', text: 'You know it is reliable when they provide sources' }
@@ -62,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
             avatar: 'media/Profilepic_qualitycontent.jpg',
             mediaSrc: ['media/Reel4_robot.mp4'],
             caption: 'Now I lost all of my use cases 😭🤣',
-            likes: 747.927,
+            likes: '747.927',
             comments: [
                 { user: 'mitchmacmillan89', text: 'It needs a coffee in the other hand ☕️' },
                 { user: 'sneek071', text: 'I don’t see any beers' }
